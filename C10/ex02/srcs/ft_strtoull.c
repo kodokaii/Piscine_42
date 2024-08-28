4f6ab232af33b38a4951628b323754b6cb495c45
kodokaii <kodokai.featheur@gmail.com>
2024-08-28 21:39:08 +0200

    hexdump print buffer

--- C10/ex02/srcs/ft_strtoull.c
+++ C10/ex02/srcs/ft_strtoull.c
@@ -6,34 +6,29 @@
 /*   By: nlaerema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2023/07/18 13:13:03 by nlaerema         ###   ########.fr       */
+/*   Updated: 2024/08/20 11:25:22 by nlaerema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
+#include <limits.h>
 
 unsigned long long	ft_strtoull(char *str, char **end)
 {
 	unsigned long long	res;
-	int					sign;
 
 	res = 0;
-	sign = 1;
 	*end = str;
 	while (('\t' <= **end && **end <= '\r') || **end == ' ')
 		(*end)++;
-	if (**end == '+' || **end == '-')
-	{
-		if (**end == '-')
-			sign *= -1;
+	if (**end == '+')
 		(*end)++;
-	}
 	while ('0' <= **end && **end <= '9')
 	{
-		res = res * 10 + **end - '0';
 		if (res * 10 + **end - '0' < res)
 			errno = ERANGE;
+		res = res * 10 + **end - '0';
 		(*end)++;
 	}
-	return (res * sign);
+	return (res);
 }
--- C10/ex03/includes/ft_hexdump.h
+++ C10/ex03/includes/ft_hexdump.h
@@ -6,7 +6,7 @@
 /*   By: nlaerema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2023/07/26 16:36:00 by nlaerema         ###   ########.fr       */
+/*   Updated: 2024/08/28 21:29:03 by nlaerema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@
 # include <stdio.h>
 # include <string.h>
 
+# define BUF_SIZE 4096
+
 # define NO_OPTION 0
 # define C_OPTION 1
 
@@ -37,12 +39,20 @@ typedef struct s_hexbuf
 	int		is_same;
 }	t_hexbuf;
 
-void	ft_hexdump_error(char *file_path, char *prog_name);
-int		ft_find_option(int *argc, char *argv[], char *option);
-void	ft_print_hexdump(t_hexbuf *hexbuf, int option);
-void	ft_print_addr(unsigned int addr, int option);
-void	ft_putoc(t_oc c);
-void	*ft_memcpy(void *dest, void *src, size_t n);
-int		ft_memcmp(void *s1, void *s2, int n);
+typedef struct s_putoc_buf
+{
+	t_oc	buf[BUF_SIZE];
+	size_t	size;
+}	t_putoc_buf;
+
+void		ft_hexdump_error(char *file_path, char *prog_name);
+int			ft_find_option(int *argc, char *argv[], char *option);
+void		ft_print_hexdump(t_hexbuf *hexbuf, int option);
+void		ft_print_addr(unsigned int addr, int option);
+t_putoc_buf	*get_putoc_buf(void);
+void		ft_putoc(t_oc c);
+void		ft_cleanup(void) __attribute__ ((destructor));
+void		*ft_memcpy(void *dest, void *src, size_t n);
+int			ft_memcmp(void *s1, void *s2, int n);
 
 #endif
